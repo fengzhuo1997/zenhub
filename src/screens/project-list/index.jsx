b1741d1f3adbf7dd5ad1adac889c3f743112192d
fengzhuo <fengzo@cn.ibm.com>
2023-03-02 16:40:42 +0800

    feat: 添加useMount和useDebounce的Custome Hook

--- src/screens/project-list/index.jsx
+++ src/screens/project-list/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { SearchPanel } from "./search-panel";
 import { List } from "./list";
 import { useEffect, useState } from "react";
-import { cleanObject } from "../../utils";
+import { cleanObject, useMount, useDebounce } from "../../utils";
 import * as qs from "qs"
 
 export const ProjectListScreen = () => {
@@ -12,22 +12,23 @@ export const ProjectListScreen = () => {
   });
   const [list, setList] = useState([]);
   const [users, setUsers] = useState([]);
+  const debounceParam = useDebounce(param, 2000)
   const apiUrl = process.env.REACT_APP_API_URL;
   useEffect(() => {
-    fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(param))}`).then(async (response) => {
+    fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(debounceParam))}`).then(async (response) => {
       if (response.ok) {
         setList(await response.json());
       }
     });
-  }, [param]);
+  }, [debounceParam]);
 
-  useEffect(() => {
+  useMount(() => {
     fetch(`${apiUrl}/users`).then(async (response) => {
       if (response.ok) {
         setUsers(await response.json());
       }
     });
-  }, []);
+  });
 
   return (
     <div>
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react"
+
 // 排除0的情况，判断值非unfefined, null
 export const isFalse = (value) => value === 0 ? false : !value
 
@@ -12,4 +14,20 @@ export const cleanObject = (object) => {
   return result
 }
 
-export default cleanObject;
+export const useMount = (callback) => {
+  useEffect(() => {
+    callback()
+  }, [])
+}
+
+// 目的是延迟修改目标值value
+export const useDebounce = (value, delay) => {
+  const [debounceValue, setDebounceValue] = useState(value)
+  useEffect(() => {
+    // 每次在value变化后，设置一个新的定时器，
+    const timeout = setTimeout(() => setDebounceValue(value), delay)
+    // 每次在上一个useEffect处理完运行。第一次设置完的timeout被第二个清理
+    return () => clearTimeout(timeout)
+  }, [value, delay])
+  return debounceValue
+}
