967df9e7016d58a9428dc5157625aa2a4fe7fcf4
fengzhuo <fengzo@cn.ibm.com>
2023-03-06 15:10:25 +0800

    chore: 添加TS类型定义

--- src/screens/project-list/index.tsx
+++ src/screens/project-list/index.tsx
@@ -3,7 +3,7 @@ import { SearchPanel } from "./search-panel";
 import { List } from "./list";
 import { useEffect, useState } from "react";
 import { cleanObject, useMount, useDebounce } from "../../utils";
-import * as qs from "qs"
+import * as qs from "qs";
 
 export const ProjectListScreen = () => {
   const [param, setParam] = useState({
@@ -12,7 +12,7 @@ export const ProjectListScreen = () => {
   });
   const [list, setList] = useState([]);
   const [users, setUsers] = useState([]);
-  const debounceParam = useDebounce(param, 2000)
+  const debounceParam = useDebounce(param, 500)
   const apiUrl = process.env.REACT_APP_API_URL;
   useEffect(() => {
     fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(debounceParam))}`).then(async (response) => {
---
+++ src/utils/index.ts
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react";
+
+// 排除0的情况，判断值非unfefined, null
+export const isFalse = (value: any) => (value === 0 ? false : !value);
+
+export const cleanObject = (object: any) => {
+  const result = { ...object };
+  Object.keys(result).forEach((key) => {
+    const value = result[key];
+    if (isFalse(value)) {
+      delete result[key];
+    }
+  });
+  return result;
+};
+
+export const useMount = (callback: () => void) => {
+  useEffect(() => {
+    callback();
+  }, []);
+};
+
+// 目的是延迟修改目标值value
+export const useDebounce = (value: any, delay?: number) => {
+  const [debounceValue, setDebounceValue] = useState(value);
+  useEffect(() => {
+    // 每次在value变化后，设置一个新的定时器，
+    const timeout = setTimeout(() => setDebounceValue(value), delay);
+    // 每次在上一个useEffect处理完运行。第一次设置完的timeout被第二个清理
+    return () => clearTimeout(timeout);
+  }, [value, delay]);
+  return debounceValue;
+};
